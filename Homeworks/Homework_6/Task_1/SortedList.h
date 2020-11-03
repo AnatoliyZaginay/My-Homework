413ad0110ec2f67ae23d697972b67184ef4854b9
AnatoliyZaginai <arkey7724@gmail.com>
2020-11-03 03:25:32 +0300

    Added comments to functions

--- Homeworks/Homework_6/Task_1/SortedList.h
+++ Homeworks/Homework_6/Task_1/SortedList.h
@@ -13,14 +13,20 @@ struct List
 	struct ListElement* head;
 };
 
+//Создаёт список
 struct List* createList(void);
 
+//Проверяет, содержится ли в списке хотя бы один элемент
 bool isEmpty(struct List* list);
 
+//Добавляет в список новый элемент, сохраняя сортированность
 void add(struct List* list, int value);
 
+//Удаляет из списка элемент по значению, возращает false в случае если элемент с таким значением не был найден
 bool delete(struct List* list, int value);
 
+//Удаляет список
 void deleteList(struct List** list);
 
+//Выводит значения всех элементов списка
 void printList(struct List* list);
--- Homeworks/Homework_6/Task_1/SortedListTests.h
+++ Homeworks/Homework_6/Task_1/SortedListTests.h
@@ -2,4 +2,5 @@
 
 #include <stdbool.h>
 
+//Проверяет, все ли функции сортированного списка работают правильно
 bool sortedListTest(void);
