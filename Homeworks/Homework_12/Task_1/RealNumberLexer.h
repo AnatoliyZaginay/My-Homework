99b8b5e44f8e1b558ad44cc901d39632e3856d18
AnatoliyZaginai <arkey7724@gmail.com>
2020-12-25 23:01:20 +0300

    Added task 12.1 with tests

---
+++ Homeworks/Homework_12/Task_1/RealNumberLexer.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdbool.h>
+
+bool isRealNumber(char* line);
---
+++ Homeworks/Homework_12/Task_1/Task_1.c
@@ -0,0 +1,27 @@
+﻿#include "RealNumberLexer.h"
+#include "RealNumberLexerTest.h"
+#include <stdio.h>
+#include <locale.h>
+
+#define LENGTH 100
+
+void main(void)
+{
+	if (!isRealNumberTest())
+	{
+		printf("Tests failed");
+		return;
+	}
+	setlocale(LC_ALL, "Rus");
+	printf("Введите последовательность символов: ");
+	char line[LENGTH] = "";
+	scanf("%s", &line);
+	if (isRealNumber(line))
+	{
+		printf("Введённая последовательность является вещественным числом");
+	}
+	else
+	{
+		printf("Введённая последовательность не является вещественным числом");
+	}
+}
